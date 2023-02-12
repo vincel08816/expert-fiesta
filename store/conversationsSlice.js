d04e94a7193c12cdac6d2023eb6223f713b7e429
Vincent Lee <vince.l08816@gmail.com>
2023-02-12 01:53:31 -0500

    code modal

--- store/conversationsSlice.js
+++ store/conversationsSlice.js
@@ -3,23 +3,28 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   conversations: [],
   selected: -1,
+  loadingConversations: false,
 };
 
 const conversationsSlice = createSlice({
   name: "conversations",
   initialState,
   reducers: {
-    setConversations: (state, action) => {
+    setConversations(state, action) {
+      state.loadingConversations = false;
       state.conversations = action.payload;
     },
-    prependConversation: (state, action) => {
+    prependConversation(state, action) {
       state.conversations = [action.payload, ...state.conversations];
     },
-    setSelected: (state, action) => {
+    setSelected(state, action) {
       state.selected = action.payload;
     },
-    getSelectedId: (state) => {
+    getSelectedId(state) {
       return state.conversations[state.selected]?._id;
+    },
+    stopLoadingConversations(state) {
+      state.loadingConversations = false;
     },
   },
 });
@@ -29,6 +34,7 @@ export const {
   prependConversation,
   setSelected,
   getSelectedId,
+  stopLoadingConversations,
 } = conversationsSlice.actions;
 
 export const conversationsReducer = conversationsSlice.reducer;
